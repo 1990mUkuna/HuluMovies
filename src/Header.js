7f7fe60392ac1c6bba48355761e28d60fde7a628
Freeman Mukuna <33888420+1990mUkuna@users.noreply.github.com>
2020-08-26 12:25:33 +0200

    Hulu hearder are complete

--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,53 @@
 import React from 'react';
 import "./Header.css";
+import HomeIcon from '@material-ui/icons/Home';
+import FlashOnIcon from '@material-ui/icons/FlashOn';
+import LiveTvIcon from '@material-ui/icons/LiveTv';
+import VideoLibraryIcon from '@material-ui/icons/VideoLibrary';
+import SearchIcon from '@material-ui/icons/Search';
+import PersonIcon from '@material-ui/icons/Person';
 
 function Header() {
     return (
 
         <div className='header'>
+        <div className="header_icons">
+           <div className="header_icon">
+           <HomeIcon />
+           <p>Home</p>
+           </div>
+           <div className="header_icon">
+           <FlashOnIcon />
+           <p>Trending</p>
+           </div>
+           <div className="header_icon">
+           <LiveTvIcon />
+           <p>Verified</p>
+           </div>
+           <div className="header_icon">
+           <VideoLibraryIcon />
+           <p>Collections</p>
+           </div>
+           <div className="header_icon">
+           <SearchIcon />
+           <p>Search</p>
+           </div>
+           <div className="header_icon">
+           <PersonIcon />
+           <p>Account</p>
+           </div>
+           
+           
             
+            
+           
+           
+
         </div>
-    )
+        <img src="https://press.hulu.com/wp-content/uploads/2020/02/hulu-white.png" alt=""/> 
+        </div>
+       
+    );
 }
 
 export default Header
